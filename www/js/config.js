50ccb5b2958c1a555ab4f18acaa38a03feef4f52
Richin Varghese <richin1990@gmail.com>
2019-01-10 11:30:16 +0530

    updated android

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={
+	'ApiUrl':"http://eyh-delivery.akstech.com.sg/driver/api",
+	'DialogDefaultTitle':"DriverApp",
+	'mapboxToken' : '',
+	'APIHasKey':"eyhhashkey",
+	'debug': false
+};
