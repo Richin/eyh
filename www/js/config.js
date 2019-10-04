a6c1e4927aa037031ad75f8868e0dcbabaadf31e
Richin Varghese <richin1990@gmail.com>
2019-10-04 20:12:30 +0530

    no message

--- www/js/config.js
+++ www/js/config.js
@@ -1,9 +1,9 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={
-	'ApiUrl':"http://eyh-delivery.akstech.com.sg/driver/api",
+	'ApiUrl':"http://mobilecity-shopping-delivery.akstech.com.sg/?ng=api/v3/",
 	'DialogDefaultTitle':"DriverApp",
 	'mapboxToken' : '',
-	'APIHasKey':"eyhhashkey",
-	'debug': false
+	'APIHasKey':"w1o3sdsfvo4eandux30mdr3b70hb4a1fozmd267j",
+	'debug': true
 };
